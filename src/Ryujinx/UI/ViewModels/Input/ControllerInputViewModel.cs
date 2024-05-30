d2bb580aea4c8b08386d4cc7800f899d85e71d85
MutantAura <domw0401@gmail.com>
2024-05-30 01:01:18 +0100

    Initial implementation of analog stick visualization.

--- src/Ryujinx/UI/ViewModels/Input/ControllerInputViewModel.cs
+++ src/Ryujinx/UI/ViewModels/Input/ControllerInputViewModel.cs
@@ -3,14 +3,31 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using FluentAvalonia.UI.Controls;
 using Ryujinx.Ava.UI.Helpers;
+using Ryujinx.Ava.Input;
 using Ryujinx.Ava.UI.Models.Input;
 using Ryujinx.Ava.UI.Views.Input;
+using Ryujinx.Input;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Ryujinx.Ava.UI.ViewModels.Input
 {
     public partial class ControllerInputViewModel : BaseModel
     {
         [ObservableProperty] private GamepadInputConfig _config;
+        
+        private const int StickUiPollMs = 50; // Milliseconds per poll.
+        private const float CanvasCenterOffset = 75f/2f;
+        private const int StickScaleFactor = 30;
+
+        private IGamepad _selectedGamepad;
+        
+        // Offset from origin for UI stick visualization.
+        private (float, float) _uiStickLeft;
+        private (float, float) _uiStickRight;
+
+        internal CancellationTokenSource _pollTokenSource = new();
+        private CancellationToken _pollToken;
 
         private bool _isLeft;
         public bool IsLeft
@@ -39,8 +56,41 @@ namespace Ryujinx.Ava.UI.ViewModels.Input
         public bool HasSides => IsLeft ^ IsRight;
 
         [ObservableProperty] private SvgImage _image;
-
+        
         public InputViewModel ParentModel { get; }
+        
+        public (float, float) UiStickLeft
+        {
+            get => (_uiStickLeft.Item1 * StickScaleFactor, _uiStickLeft.Item2 * StickScaleFactor);
+            set
+            {
+                _uiStickLeft = value;
+
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(UiStickRightX));
+                OnPropertyChanged(nameof(UiStickRightY));
+            }
+        }
+        
+        public (float, float) UiStickRight
+        {
+            get => (_uiStickRight.Item1 * StickScaleFactor, _uiStickRight.Item2 * StickScaleFactor);
+            set
+            {
+                _uiStickRight = value;
+
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(UiStickLeftX));
+                OnPropertyChanged(nameof(UiStickLeftY));
+            }
+        }
+
+        public float canvasCenter => CanvasCenterOffset;
+
+        public float UiStickLeftX => UiStickLeft.Item1 + CanvasCenterOffset;
+        public float UiStickLeftY => UiStickLeft.Item2 + CanvasCenterOffset;
+        public float UiStickRightX => UiStickRight.Item1 + CanvasCenterOffset;
+        public float UiStickRightY => UiStickRight.Item2 + CanvasCenterOffset;
 
         public ControllerInputViewModel(InputViewModel model, GamepadInputConfig config)
         {
@@ -48,6 +98,11 @@ namespace Ryujinx.Ava.UI.ViewModels.Input
             model.NotifyChangesEvent += OnParentModelChanged;
             OnParentModelChanged();
             Config = config;
+
+            _pollTokenSource = new();
+            _pollToken = _pollTokenSource.Token;
+
+            Task.Run(() => PollSticks(_pollToken));
         }
 
         public async void ShowMotionConfig()
@@ -59,7 +114,7 @@ namespace Ryujinx.Ava.UI.ViewModels.Input
         {
             await RumbleInputView.Show(this);
         }
-
+        
         public RelayCommand LedDisabledChanged => Commands.Create(() =>
         {
             if (!Config.EnableLedChanging) return;
@@ -69,6 +124,24 @@ namespace Ryujinx.Ava.UI.ViewModels.Input
             else
                 ParentModel.SelectedGamepad.SetLed(Config.LedColor.ToUInt32());
         });
+
+        private async Task PollSticks(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                _selectedGamepad = ParentModel.SelectedGamepad;
+
+                if (_selectedGamepad != null && _selectedGamepad is not AvaloniaKeyboard)
+                {
+                    UiStickLeft = _selectedGamepad.GetStick(StickInputId.Left);
+                    UiStickRight = _selectedGamepad.GetStick(StickInputId.Right);
+                }
+
+                await Task.Delay(StickUiPollMs);
+            }
+
+            _pollTokenSource.Dispose();
+        }
 
         public void OnParentModelChanged()
         {
