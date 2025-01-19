f2f099bddb946f785dbb1d2d2857c96d4633b0c8
Evan Husted <greem@greemdev.net>
2025-01-19 12:46:32 -0600

    remove Async suffixes; they're factory methods not actual async methods.

--- src/Ryujinx/UI/Helpers/Commands.cs
+++ src/Ryujinx/UI/Helpers/Commands.cs
@@ -17,32 +17,32 @@ namespace Ryujinx.Ava.UI.Helpers
         public static RelayCommand<T> CreateConditionalWithArg<T>(Action<T?> action, Predicate<T?> canExecute)
             => new(action, canExecute);
 
-        public static AsyncRelayCommand CreateAsync(Func<Task> action)
+        public static AsyncRelayCommand Create(Func<Task> action)
             => new(action, AsyncRelayCommandOptions.None);
-        public static AsyncRelayCommand CreateConcurrentAsync(Func<Task> action)
+        public static AsyncRelayCommand CreateConcurrent(Func<Task> action)
             => new(action, AsyncRelayCommandOptions.AllowConcurrentExecutions);
-        public static AsyncRelayCommand CreateSilentFailAsync(Func<Task> action)
+        public static AsyncRelayCommand CreateSilentFail(Func<Task> action)
             => new(action, AsyncRelayCommandOptions.FlowExceptionsToTaskScheduler);
         
-        public static AsyncRelayCommand<T> CreateWithArgAsync<T>(Func<T?, Task> action)
+        public static AsyncRelayCommand<T> CreateWithArg<T>(Func<T?, Task> action)
             => new(action, AsyncRelayCommandOptions.None);
-        public static AsyncRelayCommand<T> CreateConcurrentWithArgAsync<T>(Func<T?, Task> action)
+        public static AsyncRelayCommand<T> CreateConcurrentWithArg<T>(Func<T?, Task> action)
             => new(action, AsyncRelayCommandOptions.AllowConcurrentExecutions);
-        public static AsyncRelayCommand<T> CreateSilentFailWithArgAsync<T>(Func<T?, Task> action)
+        public static AsyncRelayCommand<T> CreateSilentFailWithArg<T>(Func<T?, Task> action)
             => new(action, AsyncRelayCommandOptions.FlowExceptionsToTaskScheduler);
 
-        public static AsyncRelayCommand CreateConditionalAsync(Func<Task> action, Func<bool> canExecute)
+        public static AsyncRelayCommand CreateConditional(Func<Task> action, Func<bool> canExecute)
             => new(action, canExecute, AsyncRelayCommandOptions.None);
-        public static AsyncRelayCommand CreateConcurrentConditionalAsync(Func<Task> action, Func<bool> canExecute)
+        public static AsyncRelayCommand CreateConcurrentConditional(Func<Task> action, Func<bool> canExecute)
             => new(action, canExecute, AsyncRelayCommandOptions.AllowConcurrentExecutions);
-        public static AsyncRelayCommand CreateSilentFailConditionalAsync(Func<Task> action, Func<bool> canExecute)
+        public static AsyncRelayCommand CreateSilentFailConditional(Func<Task> action, Func<bool> canExecute)
             => new(action, canExecute, AsyncRelayCommandOptions.FlowExceptionsToTaskScheduler);
         
-        public static AsyncRelayCommand<T> CreateConditionalWithArgAsync<T>(Func<T?, Task> action, Predicate<T?> canExecute)
+        public static AsyncRelayCommand<T> CreateConditionalWithArg<T>(Func<T?, Task> action, Predicate<T?> canExecute)
             => new(action, canExecute, AsyncRelayCommandOptions.None);
-        public static AsyncRelayCommand<T> CreateConcurrentConditionalWithArgAsync<T>(Func<T?, Task> action, Predicate<T?> canExecute)
+        public static AsyncRelayCommand<T> CreateConcurrentConditionalWithArg<T>(Func<T?, Task> action, Predicate<T?> canExecute)
             => new(action, canExecute, AsyncRelayCommandOptions.AllowConcurrentExecutions);
-        public static AsyncRelayCommand<T> CreateSilentFailConditionalWithArgAsync<T>(Func<T?, Task> action, Predicate<T?> canExecute)
+        public static AsyncRelayCommand<T> CreateSilentFailConditionalWithArg<T>(Func<T?, Task> action, Predicate<T?> canExecute)
             => new(action, canExecute, AsyncRelayCommandOptions.FlowExceptionsToTaskScheduler);
     }
 }
