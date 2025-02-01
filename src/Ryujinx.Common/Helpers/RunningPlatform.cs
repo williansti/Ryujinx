2c9a26c11c99d5a084c24098f1ee11b65489aea6
Evan Husted <greem@greemdev.net>
2025-02-01 03:29:24 -0600

    misc: chore: Regular Architecture bool properties in RunningPlatform without OS constraint

--- src/Ryujinx.Common/Helpers/RunningPlatform.cs
+++ src/Ryujinx.Common/Helpers/RunningPlatform.cs
@@ -10,14 +10,18 @@ namespace Ryujinx.Common.Helper
         public static bool IsMacOS => OperatingSystem.IsMacOS();
         public static bool IsWindows => OperatingSystem.IsWindows();
         public static bool IsLinux => OperatingSystem.IsLinux();
+        
+        public static bool IsArm => RuntimeInformation.OSArchitecture is Architecture.Arm64;
+        
+        public static bool IsX64 => RuntimeInformation.OSArchitecture is Architecture.X64;
 
-        public static bool IsIntelMac => IsMacOS && RuntimeInformation.OSArchitecture is Architecture.X64;
-        public static bool IsArmMac => IsMacOS && RuntimeInformation.OSArchitecture is Architecture.Arm64;
+        public static bool IsIntelMac => IsMacOS && IsX64;
+        public static bool IsArmMac => IsMacOS && IsArm;
         
-        public static bool IsX64Windows => IsWindows && (RuntimeInformation.OSArchitecture is Architecture.X64);
-        public static bool IsArmWindows => IsWindows && (RuntimeInformation.OSArchitecture is Architecture.Arm64);
+        public static bool IsX64Windows => IsWindows && IsX64;
+        public static bool IsArmWindows => IsWindows && IsArm;
         
-        public static bool IsX64Linux => IsLinux && (RuntimeInformation.OSArchitecture is Architecture.X64);
-        public static bool IsArmLinux => IsLinux && (RuntimeInformation.OSArchitecture is Architecture.Arm64);
+        public static bool IsX64Linux => IsLinux && IsX64;
+        public static bool IsArmLinux => IsLinux && IsArmMac;
     }
 }
