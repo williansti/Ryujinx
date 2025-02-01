ad9d6588e8e7a91e6185c11e8824b944a610bdb5
Evan Husted <greem@greemdev.net>
2025-02-01 14:11:35 -0600

    misc: chore: Collapse HLE swkbd character validation utils into a single class

--- src/Ryujinx.HLE/HOS/Applets/SoftwareKeyboard/NumericCharacterValidation.cs
+++
@@ -1,9 +0,0 @@
-using Ryujinx.Common.Helper;
-
-namespace Ryujinx.HLE.HOS.Applets.SoftwareKeyboard
-{
-    public static class NumericCharacterValidation
-    {
-        public static bool IsNumeric(char value) => Patterns.Numeric.IsMatch(value.ToString());
-    }
-}
