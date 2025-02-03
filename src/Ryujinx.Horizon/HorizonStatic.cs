d8549f687b08c317d1fbb82d8ce1fed2cc8daf6a
Evan Husted <greem@greemdev.net>
2025-02-03 17:04:11 -0600

    misc: chore: convert split ThreadStatic fields & property getter/setters into a single property with [field: ThreadStatic]

--- src/Ryujinx.Horizon/HorizonStatic.cs
+++ src/Ryujinx.Horizon/HorizonStatic.cs
@@ -17,27 +17,21 @@ namespace Ryujinx.Horizon
             }.Start();
         
         public static event Action<MessagePackObject> PlayReport;
-        
-        [ThreadStatic]
-        private static HorizonOptions _options;
 
-        [ThreadStatic]
-        private static ISyscallApi _syscall;
+        [field: ThreadStatic]
+        public static HorizonOptions Options { get; private set; }
 
-        [ThreadStatic]
-        private static IVirtualMemoryManager _addressSpace;
+        [field: ThreadStatic]
+        public static ISyscallApi Syscall { get; private set; }
 
-        [ThreadStatic]
-        private static IThreadContext _threadContext;
+        [field: ThreadStatic]
+        public static IVirtualMemoryManager AddressSpace { get; private set; }
 
-        [ThreadStatic]
-        private static int _threadHandle;
+        [field: ThreadStatic]
+        public static IThreadContext ThreadContext { get; private set; }
 
-        public static HorizonOptions Options => _options;
-        public static ISyscallApi Syscall => _syscall;
-        public static IVirtualMemoryManager AddressSpace => _addressSpace;
-        public static IThreadContext ThreadContext => _threadContext;
-        public static int CurrentThreadHandle => _threadHandle;
+        [field: ThreadStatic]
+        public static int CurrentThreadHandle { get; private set; }
 
         public static void Register(
             HorizonOptions options,
@@ -46,11 +40,11 @@ namespace Ryujinx.Horizon
             IThreadContext threadContext,
             int threadHandle)
         {
-            _options = options;
-            _syscall = syscallApi;
-            _addressSpace = addressSpace;
-            _threadContext = threadContext;
-            _threadHandle = threadHandle;
+            Options = options;
+            Syscall = syscallApi;
+            AddressSpace = addressSpace;
+            ThreadContext = threadContext;
+            CurrentThreadHandle = threadHandle;
         }
     }
 }
