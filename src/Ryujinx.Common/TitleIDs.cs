11e4d8f970e8a8100210072755b929396580360c
shinra-electric <50119606+shinra-electric@users.noreply.github.com>
2025-02-03 07:36:58 +0000

    Add the JP version of P5R to the list of Metal games (#612)

--- src/Ryujinx.Common/TitleIDs.cs
+++ src/Ryujinx.Common/TitleIDs.cs
@@ -47,7 +47,8 @@ namespace Ryujinx.Common
             "01006f8002326000", // Animal Crossings: New Horizons
             "01009bf0072d4000", // Captain Toad: Treasure Tracker
             "01009510001ca000", // Fast RMX
-            "01005CA01580E000", // Persona 5 Royale
+            "01005CA01580E000", // Persona 5 Royal
+            "0100b880154fc000", // Persona 5 The Royal (Japan)
             "010015100b514000", // Super Mario Bros. Wonder
             "0100000000010000", // Super Mario Odyssey
 
